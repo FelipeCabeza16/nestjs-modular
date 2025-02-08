08301b0ec4dc7ad4b85164e3e25b66276ae420de
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-02-08 15:09:21 -0500

    feat: call todos async json typicode to nest v 7

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, HttpService, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'APP_NAME',
       useValue: 'NestJS App',
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
