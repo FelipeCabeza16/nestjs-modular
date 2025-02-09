32355b2486dfd65977c1910aa612c7d217d0c267
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-02-08 20:40:20 -0500

    feat: add environments config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('APP_NAME') private appName: string,
     @Inject('TASKS') private tasks: any[],
+    private config: ConfigService,
   ) {}
   getHello(): string {
-    console.log(this.tasks);
-    return `Hello World! This is ${this.appName}`;
+    const apiKey = this.config.get<string>('API_KEY');
+    const name = this.config.get('DATABASE_NAME');
+    return `Hello World! ${this.appName} ${this.tasks.length} ${apiKey} ${name}`;
   }
 }
