2813ede331ad992ccacb22babb01b998c44a872e
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-02-08 21:20:50 -0500

    feat: class 12 add joi for env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { HttpModule, HttpService, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
 
 import { environments } from './environments';
 import config from './config';
+
 
 @Module({
   imports: [
@@ -19,6 +22,11 @@ import config from './config';
       envFilePath: environments[process.env.NODE_ENV] || '.env', // 👈
       load: [config], // 👈
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AppController],
