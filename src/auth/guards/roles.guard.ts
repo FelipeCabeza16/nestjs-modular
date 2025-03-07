291c9f32b4f5767e00108d1d0d36aac267a4d766
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-03-07 17:07:00 -0500

    feat: auth-class-15 roles admin and customer to read create products guard validator

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { PayloadToken } from '../models/token.model';
+import { Role } from '../models/roles.model';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+
+    console.log('user', user);
+    console.log('requiredRoles', requiredRoles);
+    return requiredRoles.some((role) => user.role?.includes(role));
+  }
+}
--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -25,8 +25,11 @@ import {
 import { ProductsService } from '../services/products.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/models/roles.model';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
@@ -51,6 +54,7 @@ export class ProductsController {
     return this.productsService.findOne(productId);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
