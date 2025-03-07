59bc55040ed232e84917034d4985c82dadf0d23a
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-03-07 16:31:35 -0500

    feat: auth-class-14 extending auth-guard

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -15,7 +15,6 @@ import {
 } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
-import { Response } from 'express';
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import {
   CreateProductDto,
@@ -24,14 +23,16 @@ import {
 } from '../dtos/products.dtos';
 
 import { ProductsService } from '../services/products.service';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Public } from 'src/auth/decorators/public.decorator';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
+  @Public()
   @Get()
   @ApiOperation({ summary: 'List of products' })
   getProducts(@Query() params: FilterProductsDto) {
@@ -43,6 +44,7 @@ export class ProductsController {
     return `yo soy un filter`;
   }
 
+  @Public()
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', ParseIntPipe) productId: number) {
